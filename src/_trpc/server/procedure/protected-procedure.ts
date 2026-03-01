4d76ba42275526b647fc794f58c7c3d9dd1df392
shvgpt-070208 <shvgpt070208@gmail.com>
2026-03-01 14:23:44 +0530

    feat: procedures created

---
+++ src/_trpc/server/procedure/protected-procedure.ts
@@ -0,0 +1,20 @@
+import { getServerSession } from "@/lib/auth-utils";
+import { baseProcedure } from "../../init";
+import { TRPCError } from "@trpc/server";
+
+export const protectedProcedure = baseProcedure.use(async ({ ctx, next }) => {
+  const session = await getServerSession();
+
+  if (!session) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "you are not authorized to access this resource",
+    });
+  }
+  return next({
+    ctx: {
+      ...ctx,
+      session,
+    },
+  });
+});
